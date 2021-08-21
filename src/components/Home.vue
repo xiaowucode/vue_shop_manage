39be6c623100702020cf57b2cd9178249174c612
xiaowucode <727555063@qq.com>
2021-08-21 18:46:44 +0800

    day02完成登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    // 退出登录，删除token值，并跳转到登录页面
+    logou() {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
@@ -9,12 +10,22 @@ const routes = [
   // 路由重定向
   { path: '/', redirect: '/login' },
 
-  { path: '/login', component: Login }
+  { path: '/login', component: Login },
+
+  { path: '/home', component: Home }
 
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') return next()
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
 })
 
 export default router
