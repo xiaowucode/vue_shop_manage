813fcc00c8d2948fd0d54d6fe3298026187dfa55
xiaowucode <727555063@qq.com>
2021-09-12 18:36:49 +0800

    report功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import GoodsList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Order from '../components/orders/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -35,6 +36,7 @@ const routes = [
       { path: '/goods', component: GoodsList },
       { path: '/goods/add', component: Add },
       { path: '/orders', component: Order },
+      { path: '/reports', component: Report },
     ]
   },
 ]
